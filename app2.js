0cf776c2ae4ae444af69f82b77d220784a979f61
Piotr Frydryszak <piotr.frydryszak@gmail.com>
2019-01-04 12:11:00 +0100

    Objects Literals

---
+++ app2.js
@@ -0,0 +1,12 @@
+//objects and objects literal
+var person = {
+  firstname: "John",
+  lastname: "Doe",
+  greet: function(){
+    console.log("Hello, " + this.firstname + " " + this.lastname);
+    }
+};
+
+person.greet();
+console.log(person['firstname']);
+  

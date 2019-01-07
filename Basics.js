94ce89f7e44598be8e39dddf5dffa1f069532cad
Sulabasana <piotr.frydryszak@gmail.com>
2019-01-07 18:25:31 +0100

    More about require

---
+++ Basics.js
@@ -0,0 +1,36 @@
+// var a = 1;
+// var b = 2;
+// var c = a + b;
+
+// console.log(c);
+
+//function statment
+
+// function greet(){
+//     console.log('hi')
+// }
+
+// greet();
+
+
+// //function are first-class
+// function logGreeting(fn){
+//     fn();
+// }
+
+// logGreeting(greet);
+
+
+// //fumction expression
+// var greetMe = function(){
+//     console.log("Hi Tony")
+// }
+// greetMe();
+
+// //it is first-class
+// logGreeting(greetMe);
+
+// //use a function expression on the fly
+// logGreeting(function(){
+//     console.log("Hello Tony")
+// })
--- app.js
+++ app.js
@@ -1,36 +1,4 @@
-// var a = 1;
-// var b = 2;
-// var c = a + b;
+var greet = require('./greet');
 
-// console.log(c);
-
-//function statment
-
-function greet(){
-    console.log('hi')
-}
-
-greet();
-
-
-//function are first-class
-function logGreeting(fn){
-    fn();
-}
-
-logGreeting(greet);
-
-
-//fumction expression
-var greetMe = function(){
-    console.log("Hi Tony")
-}
-greetMe();
-
-//it is first-class
-logGreeting(greetMe);
-
-//use a function expression on the fly
-logGreeting(function(){
-    console.log("Hello Tony")
-})
+greet.english();
+greet.spanish();

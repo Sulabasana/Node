1ea9d3d86d843eb0b9e2f2a6f3800c65f0fb0c8a
Sulabasana <piotr.frydryszak@gmail.com>
2019-01-13 16:27:08 +0100

    Emitter part2

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-var Emitter = require("./emitter")
+var Emitter = require("events")//removing ./ because it is built in feature
+var eventConfig = require('./config').events;
 
 var emtr = new Emitter();
 
-emtr.on("greet", function(){
+emtr.on(eventConfig.GREET, function(){
     console.log("Somwehere, someone said hello")
 })
 
-emtr.on('greet', function(){
+emtr.on(eventConfig.GREET, function(){
     console.log("A greeting occured!")
 })
 
 console.log("Hello")
 
-emtr.emit("greet");
+emtr.emit(eventConfig.GREET);
